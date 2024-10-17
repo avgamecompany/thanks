bc678851ed8e16777c46a172363aa3ccc7a8a271
avgamecompany <originalavgamecompany@gmail.com>
2024-10-17 13:29:51 +0300

    Add files via upload

---
+++ play.js
@@ -0,0 +1,11 @@
+function prime(x) {
+
+    var d=document.createElement("div");
+d.innerText =x
+document.getElementById("x").appendChild(d)
+}
+
+prime("hi everyone its aviv lazarovitch the creator of AVgame company.");
+prime("we wanted everyone to visit our sites and play our games this is the goal of the company.");
+prime("now thank you so much for visiting our sites and playing our games if you did not play our games its in this site https://itch.io/profile/avgamecompany");
+prime("© 2024 AVgame. All rights reserved");
